7e200e7886238a916f843a8d30c004c0f188c8bb
Shadab-Afzal7800 <shadabafzal7800@gmail.com>
2024-02-27 18:12:18 +0530

    fetchuser

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,6 +11,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<dynamic> users = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +23,37 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         centerTitle: true,
       ),
+      floatingActionButton:
+          ElevatedButton(onPressed: fetchUsers, child: Text('Fetch')),
+      body: ListView.builder(
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+            final user = users[index];
+            final email = user["email"];
+            final name =
+                '${user["name"]["title"]} ${user["name"]["first"]} ${user["name"]["last"]}';
+
+            final imageUrl = user["picture"]["thumbnail"];
+            return ListTile(
+              title: Text(name.toString()),
+              subtitle: Text(email),
+              leading: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.network(imageUrl),
+              ),
+            );
+          }),
     );
+  }
+
+  void fetchUsers() async {
+    const url = 'https://randomuser.me/api/?results=100';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    setState(() {
+      users = json['results'];
+    });
   }
 }
